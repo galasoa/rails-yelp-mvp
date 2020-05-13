3086406eb55e38af004812db482e06694a14dd24
Gala Anania <gsanania@gmail.com>
2020-05-13 16:56:05 +0200

    restaurants#new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,11 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  # def show
-  #   @restaurant = Restaurant.find(params[:id])
-  # end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
 end
